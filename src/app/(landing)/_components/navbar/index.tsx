2c824b402eb430aecdadecd39c81190fd87db4ef
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-13 09:03:45 +0800

    wip: create login btn and mobile menu (navbar)

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,10 +1,32 @@
+import Link from "next/link";
 import Menu from "./menu";
+import { Button } from "@/components/ui/button";
+import { LogOut, MenuIcon } from "lucide-react";
+import GlassSheet from "@/components/global/glass-sheet";
 
 const LandingPageNavBar = () => {
   return (
     <div className="w-full flex justify-between sticky top-0 items-center py-5 z-50 ">
       <p className="font-bold text-2xl">Logo</p>
       <Menu orientation="desktop" />
+      <div className="flex gap-2">
+        <Link href={"/sign-in"}>
+          <Button className="bg-theme" variant={"outline"}>
+            <LogOut />
+            Login
+          </Button>
+        </Link>
+        <GlassSheet
+          trigger={
+            <Button variant={"ghost"} className="hover:bg-transparent">
+              <MenuIcon size={30} />
+            </Button>
+          }
+          triggerClass="lg:hidden"
+        >
+          <Menu orientation="mobile" />
+        </GlassSheet>
+      </div>
     </div>
   );
 };
