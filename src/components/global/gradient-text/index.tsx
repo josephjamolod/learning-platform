75742e423f1d1efbf79ac0f6c899ef43bef8cb89
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-13 13:41:42 +0800

    wip: create "CallToAction" comp. (main page)

---
+++ src/components/global/gradient-text/index.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+type GradientTextProps = {
+  element?: "H1" | "H2";
+  children: React.ReactNode;
+  className: string;
+};
+const GradientText = ({ children, className, element }: GradientTextProps) => {
+  switch (element) {
+    case "H1":
+      return <h1 className={cn(className, "text-gradient")}>{children}</h1>;
+
+    case "H2":
+      return <h2 className={cn(className, "text-gradient")}>{children}</h2>;
+    default:
+      return <p className={cn(className, "text-gradient")}>{children}</p>;
+  }
+};
+export default GradientText;
