ad9a7ebbec0527b6827869494d7d381bcbb92c33
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-13 13:44:36 +0800

    wip: create "PricingSection" comp. (main page)

---
+++ src/components/global/backdrop-gradient/index.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+
+type BackdropGradientProps = {
+  children: React.ReactNode;
+  className?: string;
+  container?: string;
+};
+const BackdropGradient = ({
+  className,
+  children,
+  container,
+}: BackdropGradientProps) => {
+  return (
+    <div className={cn("relative w-full flex flex-col", container)}>
+      <div
+        className={cn("absolute rounded-[50%] radial--blur mx-10", className)}
+      />
+      {children}
+    </div>
+  );
+};
+export default BackdropGradient;
