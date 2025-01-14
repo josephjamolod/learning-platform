2d704621d1d638d29d86bc05b186a7e9387c3f2b
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-14 17:22:48 +0800

    feat: initialize sign-in form and oauth btn

---
+++ src/app/(auth)/oauth/page.tsx
@@ -0,0 +1,7 @@
+interface GoogleAuthButtonProps {
+  method: "signin" | "signup";
+}
+
+export default function GoogleAuthButton({}: GoogleAuthButtonProps) {
+  return <div>OauthButton</div>;
+}
---
+++ src/components/forms/sign-in-form/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+// import { useAuthSignIn } from "@/hooks/authentication";
+
+const SignInForm = () => {
+  // const { errors, isPending, onAuthenticateUser, register } = useAuthSignIn();
+  return <div>SignInForm</div>;
+};
+export default SignInForm;
