94ee493135c233a2f67dafca593f01416ff18ba7
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-15 01:07:07 +0800

    fix: fix import naming

--- src/app/(landing)/_components/navbar/menu.tsx
+++ src/app/(landing)/_components/navbar/menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card, CardContent } from "@/components/ui/card";
-import { NAV_CONSTANTS } from "@/constants";
+import { LOGO_CONSTANTS } from "@/constants";
 import { useNavigation } from "@/hooks/navigation";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
@@ -17,7 +17,7 @@ const Menu = ({ orientation }: MenuProps) => {
       return (
         <Card className="bg-themeGray border-themeGray bg-clip-padding backdop--blur__safari backdrop-filter backdrop-blur-2xl bg-opacity-60 p-1 lg:flex hidden rounded-xl">
           <CardContent className="p-0 flex gap-2">
-            {NAV_CONSTANTS.landingPageMenu.map((menuItem) => {
+            {LOGO_CONSTANTS.landingPageMenu.map((menuItem) => {
               return (
                 <Link
                   href={menuItem.path}
@@ -43,7 +43,7 @@ const Menu = ({ orientation }: MenuProps) => {
     case "mobile":
       return (
         <div className="flex flex-col my-10">
-          {NAV_CONSTANTS.landingPageMenu.map((menuItem) => {
+          {LOGO_CONSTANTS.landingPageMenu.map((menuItem) => {
             return (
               <Link
                 href={menuItem.path}
