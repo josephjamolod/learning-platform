7d9622bb2465ff828432f54fd55829c76b625eb1
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-17 16:39:37 +0800

    wip: initialize StripeElements component

---
+++ src/components/global/stripe/elements.tsx
@@ -0,0 +1,7 @@
+type StripeElementsProps = {
+  children: React.ReactNode;
+};
+const StripeElements = ({ children }: StripeElementsProps) => {
+  return <div>{children}</div>;
+};
+export default StripeElements;
