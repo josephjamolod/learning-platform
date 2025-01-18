5e67083e95a825165cede3eca1bf051e5c3026db
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-18 23:24:58 +0800

    feat: create StripeElements component

--- src/components/global/stripe/elements.tsx
+++ src/components/global/stripe/elements.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useStripeElements } from "@/hooks/payment";
+import { Elements } from "@stripe/react-stripe-js";
+
 type StripeElementsProps = {
   children: React.ReactNode;
 };
 const StripeElements = ({ children }: StripeElementsProps) => {
-  return <div>{children}</div>;
+  const { stripePromise } = useStripeElements();
+
+  const promise = stripePromise();
+  return promise && <Elements stripe={promise}>{children}</Elements>;
 };
 export default StripeElements;
